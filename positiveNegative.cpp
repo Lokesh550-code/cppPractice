00cf806d94780726ceaf5940e52400e7a1bd95fb
Lokesh550-code <lok3sh2007@gmail.com>
2026-08-10 12:08:59 +0530

    cpp: program to check if a number is positive or negative

---
+++ positiveNegative.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main () {
+    
+    double a;
+    std::cout << "Enter the number: ";
+
+    if(a > 0 ){
+        std::cout << a <<" is a positive numeber" << std::endl;
+    } else {
+        std::cout << a <<" is a negative numeber" << std::endl;
+    }
+
+    return 0;
+}
