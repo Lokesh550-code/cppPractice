5a2f2bbc3790e6d4f0323c47be50f8eaa4f3be21
Lokesh550-code <lok3sh2007@gmail.com>
2026-08-10 16:19:29 +0530

    cpp: program to check if a number is even or odd

---
+++ evenOdd.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main () {
+    int num;
+
+    std::cout << "Enter the number: ";
+    std::cin >> num;
+
+    if(num % 2 == 0 ) {
+        std::cout << "The number " << num << " is even." << std::endl;
+    } else {
+        std::cout << "The number " << num << " is odd." << std::endl;
+    }
+
+    return 0;
+}
